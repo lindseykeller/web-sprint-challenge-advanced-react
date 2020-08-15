7b85c42935817423340b0c501a1ae696176fa658
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-08-14 18:40:54 -0700

    moved addToCart and removeFromCart to useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,25 +3,19 @@ import { useState } from "react";
 
 
 const useForm = () => {
-  const [cart, setCart] = useState('');
+  const [cart, setCart] = useState([]);
 
-//   const handleChanges = (e) => {
-//     console.log(e.target.name, e.target.value);
-//     setValues({
-//       ...values,
-//       [e.target.name]: e.target.value
-//     });
-//   };
 
-  // add a plant to the cart
-//   const addToCart = (plant) => {
-//     setCart([...cart, plant]);
-//   };
+
+  //add a plant to the cart
+  const addToCart = (plant) => {
+    setCart([...cart, plant]);
+  };
 
 //   // remove a plant from the cart
-//   const removeFromCart = (plant) => {
-//     setCart(cart.filter((p) => p.id !== plant.id));
-//   };
+  const removeFromCart = (plant) => {
+    setCart(cart.filter((p) => p.id !== plant.id));
+  };
 
   return [cart, setCart];
 };
